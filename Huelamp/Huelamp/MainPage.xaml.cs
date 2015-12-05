6cfb40c0ce94411f42b7e85afcffdc31c9230893
Karel van Waveren <kareljf@gmail.com>
2015-12-05 14:03:39 +0100

    Added local data storage

--- Huelamp/Huelamp/MainPage.xaml.cs
+++ Huelamp/Huelamp/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Huelamp.Models;
+using Windows.Storage;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,17 +25,26 @@ namespace Huelamp
     public sealed partial class MainPage : Page
     {
         private List<Huelampwaardes> huelampen;
+        public static ApplicationData APP_DATA = ApplicationData.Current;
+        public static ApplicationDataContainer LOCAL_SETTINGS = APP_DATA.LocalSettings;
 
         public MainPage()
         {
-            this.InitializeComponent();
-            huelampen = Huelampmanager.GetHuelampen();
+        this.InitializeComponent();
+        huelampen = Huelampmanager.GetHuelampen();
             
         }
 
-        private void Infolamp_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            
-        }
+    private void Infolamp_Tapped(object sender, TappedRoutedEventArgs e)
+    {
+
     }
+        // deze methode in de classe zetten waar je je settings invult
+        public static void SetSettings(string ip, int port, string username)
+        {
+            MainPage.LOCAL_SETTINGS.Values["ip"] = ip;
+            MainPage.LOCAL_SETTINGS.Values["port"] = port;
+            MainPage.LOCAL_SETTINGS.Values["user"] = username;
+        }
+}
 }
