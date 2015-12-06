cfd68d253635f96641c20e98c1a2be8b3529947d
Karel van Waveren <kareljf@gmail.com>
2015-12-06 14:31:31 +0100

    lamps are now added to array

--- Huelamp/Huelamp/Models/Huelampwaardes.cs
+++ Huelamp/Huelamp/Models/Huelampwaardes.cs
@@ -16,15 +16,5 @@ namespace Huelamp.Models
             public Boolean isVisible { get; set; }
         }
 
-        public class Huelampmanager
-        {
-            public static List<Huelampwaardes> GetHuelampen()
-            {
-                var huelampen = new List<Huelampwaardes>();
-                huelampen.Add(new Huelampwaardes { on = true, brightness = 255, hue = 25355, saturation = 255, naam = "lamp1"});
-                huelampen.Add(new Huelampwaardes { on = true, brightness = 125, hue = 55355, saturation = 55, naam = "lamp2"});
-                huelampen.Add(new Huelampwaardes { on = false, brightness = 12, hue = 65433, saturation = 124, naam = "lamp3" });
-                return huelampen;
-            }
-        }
+
     }
